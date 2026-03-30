6d5e304e7daae0b944eff642d0ea9c34aaff9fbf
ZaharGirl <ZaharGirl@ioiio.cc>
2026-03-30 11:48:58 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+// deno-lint-ignore-file no-sloppy-imports
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <Button>Click me</Button>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
